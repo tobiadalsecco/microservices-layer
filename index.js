e77cff366317e1ae28e55101245ef65b88ef9883
tobiadalsecco <tobia.dalsecco@gmail.com>
2015-10-05 22:33:07 -0300

    bugfix

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 module.exports.Service = require('./lib/Service');
 module.exports.Client = require('./lib/Client');
+module.exports.Pub = require('./lib/Pub');
+module.exports.Sub = require('./lib/Sub');
